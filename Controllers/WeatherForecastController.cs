1ce980f3a54c333405a353f3f8e0c11d7c40d7aa
Rajesh Satpathy <rajesh.a.s.1997@gmail.com>
2025-05-25 23:10:19 -0500

    Remove puppeteersharp implementation

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,6 +1,5 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using PuppeteerSharp;
 
 namespace TodoApi.Controllers;
 
@@ -44,102 +43,5 @@ public class WeatherForecastController : ControllerBase
             Summary = Summaries[Random.Shared.Next(Summaries.Length)] + " Post"
         })
         .ToArray();
-    }
-
-    [HttpGet(Name = "PuppeteerSharpTest")]
-    [Route("PuppeteerSharpTest")]
-    public async Task<IActionResult> PuppeteerSharpTest()
-    {
-        var url = "http://localhost:5239/WeatherForecast/PostWeatherForecast";
-        await new BrowserFetcher().DownloadAsync();
-        var browser = await Puppeteer.LaunchAsync(new LaunchOptions{ Headless = true });
-        var page = await browser.NewPageAsync();
-        await page.SetCookieAsync(new CookieParam { Name = "127-cookie", Value = "worst", Url =  url});
-
-        await page.SetRequestInterceptionAsync(true);
-        page.Request += async (sender, e) =>
-        {
-            var payload = new Payload { Method = HttpMethod.Post, PostData = "doggo" };
-            await e.Request.ContinueAsync(payload);
-        };
-
-        await page.GoToAsync(url);
-        await page.PdfAsync("./output/output.pdf");
-        return this.Ok("Test succeeded");
-    }
-
-    [HttpGet(Name = "PuppeteerSharpCustomTest")]
-    [Route("PuppeteerSharpCustomTest")]
-    public async Task<IActionResult> PuppeteerSharpCustomTest()
-    {
-        var launcherArgs = new[]
-        {
-            "--disable-background-timer-throttling",
-            "--disable-breakpad",
-            "--disable-client-side-phishing-detection",
-            "--disable-cloud-import",
-            "--disable-default-apps",
-            "--disable-dev-shm-usage",
-            "--disable-extensions",
-            "--disable-gesture-typing",
-            "--disable-hang-monitor",
-            "--disable-infobars",
-            "--disable-notifications",
-            "--disable-offer-store-unmasked-wallet-cards",
-            "--disable-offer-upload-credit-cards",
-            "--disable-popup-blocking",
-            "--disable-print-preview",
-            "--disable-prompt-on-repost",
-            "--disable-setuid-sandbox",
-            "--disable-speech-api",
-            "--disable-sync",
-            "--disable-tab-for-desktop-share",
-            "--disable-translate",
-            "--disable-voice-input",
-            "--disable-wake-on-wifi",
-            "--disk-cache-size=33554432",
-            "--enable-async-dns",
-            "--enable-simple-cache-backend",
-            "--enable-tcp-fast-open",
-            "--hide-scrollbars",
-            "--ignore-gpu-blacklist",
-            "--media-cache-size=33554432",
-            "--metrics-recording-only",
-            "--mute-audio",
-            "--no-default-browser-check",
-            "--no-first-run",
-            "--no-pings",
-            "--no-sandbox",
-            "--no-zygote",
-            "--password-store=basic",
-            "--prerender-from-omnibox=disabled",
-            "--use-gl=angle",
-            "--use-angle=swiftshader",
-            "--use-mock-keychain",
-        };
-
-        var executablePath = "D:\\TAMU_docs\\5th_and_beyond\\PuppeteerSharp\\TodoApi\\chrome\\win64-133.0.6943.126\\chrome-win64\\chrome.exe";
-
-        using var browser = await Puppeteer.LaunchAsync(new LaunchOptions
-        {
-            Headless = true,
-            ExecutablePath = executablePath,
-            AcceptInsecureCerts = true,
-            Args = launcherArgs
-        });
-
-        using var page = await browser.NewPageAsync();
-
-        var html = "<html><head></head><body>Hello World</body></html>";
-
-        await page.SetContentAsync(html);
-
-        var content = await page.GetContentAsync();
-
-        var pdfContent = await page.PdfDataAsync();
-
-        // await page.PdfAsync("./output/output1.pdf");
-        return File(pdfContent, "application/pdf", "output.pdf");
-        // return this.Ok("Test succeeded");
     }
 }
